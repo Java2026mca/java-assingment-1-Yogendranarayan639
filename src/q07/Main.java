95b3daba6a53303b5c849c6253fc966854f12227
Yogendra Narayan Pandey <yogendranarayanpandey50@gmail.com>
2026-04-07 22:02:15 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,22 +1,42 @@
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Grader {
+
+    public static void solve() {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+        int[] arr = new int[n];
 
-        // TODO: Read n integers into an array
-        //       Implement BUBBLE SORT manually (no Arrays.sort)
-        //       Print sorted array ascending, space-separated
-        //       Then print: "Swaps: X" where X = total number of swaps performed
-        //
-        // Input:
-        // 5
-        // 64 34 25 12 22
-        //
-        // Output:
-        // 12 22 25 34 64
-        // Swaps: 7
+        // Read input
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        int swaps = 0;
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swaps++;
+                }
+            }
+        }
+
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
+            if (i > 0) System.out.print(" ");
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+
+        // Print swaps
+        System.out.println("Swaps: " + swaps);
+
+        sc.close();
     }
 }
