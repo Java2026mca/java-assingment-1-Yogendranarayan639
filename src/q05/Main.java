4cd40ac9749e4756d0f54336cf5d0165ac54409f
Yogendra Narayan Pandey <yogendranarayanpandey50@gmail.com>
2026-04-07 21:42:01 +0530

    Update Main.java

--- src/q05/Main.java
+++ src/q05/Main.java
@@ -1,46 +1,16 @@
 import java.util.*;
 
 public class Main {
-
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
-    }
-
-    public static boolean isPerfect(int num) {
-        if (num <= 1) return false;
-
-        int sum = 1;
-
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                sum += i;
-                if (i != num / i) {
-                    sum += num / i;
-                }
-            }
-        }
-
-        return sum == num;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-
-        while (n-- > 0) {
-            int num = sc.nextInt();
-
-            if (isPrime(num)) {
-                System.out.println("Prime");
-            } else if (isPerfect(num)) {
-                System.out.println("Perfect");
+        while (sc.hasNext()) {
+            if (sc.hasNextDouble()) {
+                double r = sc.nextDouble();
+                double area = Math.PI * r * r;
+                System.out.printf("Area: %.2f\n", area);
             } else {
-                System.out.println("Neither");
+                sc.next(); // skip invalid input safely
             }
         }
 
